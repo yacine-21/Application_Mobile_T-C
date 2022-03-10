ba8706b470fab4b577ffa63424e8a1a3554479ac
yacine-geolane <yacinelyoubi37@gmail.com>
2022-03-10 12:28:36 +0100

    small fix

--- component/Card.js
+++ component/Card.js
@@ -250,7 +250,7 @@ const styles = StyleSheet.create({
     shadowColor: '#000',
     shadowOffset: {
       width: 0,
-      height: 2,
+      height: 3,
     },
     shadowOpacity: 0.25,
     shadowRadius: 3.84,
